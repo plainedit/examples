8fcc240c44938be9aeceeef7576747526a7b67d4
tom-sapletta-com <tom@sapletta.com>
2022-05-08 09:24:52 +0200

    update menu url

--- DOCS/PROJECTS_LOCAL.md
+++ DOCS/PROJECTS_LOCAL.md
@@ -1,3 +1,3 @@
-+ [/media/tom/projects/plainedit/examples](//media/tom/projects/plainedit/examples/)
-+ [1 # Test 1](//media/tom/projects/plainedit/examples/1/index.html)
-+ [2 # Test 2](//media/tom/projects/plainedit/examples/2/index.html)
++ [/media/tom/projects/plainedit/examples](file:///media/tom/projects/plainedit/examples/)
++ [1 # Test 1](file:///media/tom/projects/plainedit/examples/1/index.html)
++ [2 # Test 2](file:///media/tom/projects/plainedit/examples/2/index.html)
--- readme.sh
+++ readme.sh
@@ -20,7 +20,7 @@ echo "$(date +"%T.%3N") CREATE_MENU" >> $LOGS
 #
 DOMAIN=$(cat CNAME)
 echo "+ [$DOMAIN](http://$DOMAIN)" > DOCS/PROJECTS.md
-echo "+ [$LOCAL_PATH](/$LOCAL_PATH/)" > DOCS/PROJECTS_LOCAL.md
+echo "+ [$LOCAL_PATH](file://$LOCAL_PATH/)" > DOCS/PROJECTS_LOCAL.md
 #
 for FILE in */in.md; do
   line=$(head -n 1 $FILE)
@@ -29,17 +29,17 @@ for FILE in */in.md; do
   NAME=${FILE%%/*}
   URL=$DOMAIN/$NAME
   echo "+ [$NAME $line](http://$URL)" >> DOCS/PROJECTS.md
-  echo "+ [$NAME $line](/$LOCAL_PATH/$NAME/index.html)" >> DOCS/PROJECTS_LOCAL.md
+  echo "+ [$NAME $line](file://$LOCAL_PATH/$NAME/index.html)" >> DOCS/PROJECTS_LOCAL.md
 done
 
 ## combine from another sites
 echo "$(date +"%T.%3N") COMBINE_FILES" >> $LOGS
 
-echo "" >README.md
+echo "" > README.md
 for project in $PROJECT_LIST; do
   echo "$(date +"%T.%3N") COMBINE_FILE $project" >> $LOGS
-  cat $project >>README.md
-  echo "" >>README.md
+  cat $project >> README.md
+  echo "" >> README.md
 done
 # Send to github
 #git commit -m "Combine and update docs"
